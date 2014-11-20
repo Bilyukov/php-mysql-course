e6c797b5a7b6837a58f5826ef02b0c788b957dd6
gbilyuko <gbilyukov@hhogdev.com>
2014-11-20 11:35:11 +0200

    Finalized PHP lection

--- examples/04.php-basics/11.php-logical-operators-priority.php
+++ examples/04.php-basics/11.php-logical-operators-priority.php
@@ -8,7 +8,7 @@
 	if(($first_nimber == 99 || $second_nimber == 2) && ($third_nimber == 3 && $forth_nimber == 4)){
 		echo "Условието е изпълнено";
 	} else {
-		echo "Условието <strong>не е</strong> еизпълнено!" 
+		echo "Условието <strong>не е</strong> еизпълнено!";
 	}
 	
 	
