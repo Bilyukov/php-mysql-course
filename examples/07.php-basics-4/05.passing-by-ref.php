2616ea22c0c1aa50e639accda1071c0ff061090d
Georgi Bilyukov <georgi.bilyukov@gmail.com>
2014-12-01 19:34:28 +0200

    Finalized 06.php-bacics-4 lection and examples

---
+++ examples/07.php-basics-4/05.passing-by-ref.php
@@ -0,0 +1,29 @@
+﻿<!DOCTYPE html>
+<html>
+<head lang="en">
+    <meta charset="UTF-8">
+    <title>Reference</title>
+</head>
+<body>
+<?php
+	
+	
+	$number = 12;
+	
+	$multiplier = 3;
+	
+	multiply($number, $multiplier);
+	
+	echo "12 умножено по 3 = $number";
+	
+	
+	function multiply(&$number, $multiplier) {
+		$number *= $multiplier;
+	}
+
+	
+
+?>
+
+</body>
+</html>
---
+++ examples/07.php-basics-4/07.scope.php
@@ -0,0 +1,23 @@
+﻿<!DOCTYPE html>
+<html>
+<head lang="en">
+    <meta charset="UTF-8">
+    <title>Reference</title>
+</head>
+<body>
+<?php
+	
+	$a = "IT Academy";
+	
+	showA();
+	
+	function showA(){
+		global $a; 
+		
+		echo 'Стойността на променливата $а е : <strong>' . $a . '</strong>'; 
+	}
+
+?>
+
+</body>
+</html>
