333867fc7fbaffec8bbbdbef2376fa076bd66558
Georgi Bilyukov <georgi.bilyukov@gmail.com>
2014-12-01 07:24:36 +0200

    Modified 06.php-bacics-4 presentation and examples

---
+++ examples/07.php-basics-4/03.function-params.php
@@ -0,0 +1,30 @@
+﻿
+<?php
+	
+	
+	$userInfo = array(
+        'Име' => 'Георги Билюков',
+        'Потребителско Име' => 'gbilyukov',
+        'Email' => 'georgi.bilyukov@gmail.com',
+        'ID' => '123'
+    );
+	
+	echo "<table border=\"1\">";
+	
+	
+	foreach ($userInfo as $key => $value) {
+		printRow($key, $value);
+    }
+	
+	echo "</table>";
+	
+	
+	function printRow($key, $value) {
+		echo "<tr>";
+		echo "<td>" . $key . "</td>";
+		echo "<td>" . $value . "</td>";
+		echo "</tr>";
+	}
+	
+
+?>
