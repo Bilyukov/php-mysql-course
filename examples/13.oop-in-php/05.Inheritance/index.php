24eecfbf82cbaa15c82bc7a595c355a5c5dd793d
gbilyuko <gbilyukov@hhogdev.com>
2015-01-06 13:53:36 +0200

    Finalized OOP lection and examples

---
+++ examples/13.oop-in-php/05.Inheritance/index.php
@@ -0,0 +1,13 @@
+﻿<?php 
+	require_once(dirname(__FILE__).'/Pet.php');
+	require_once(dirname(__FILE__).'/Cat.php');
+	
+	$myCat = new Cat("Josefine", 2, "Persian");
+	
+	$myCat->greet();
+	$myCat->sayBreed();
+	
+	
+	
+
+?>
