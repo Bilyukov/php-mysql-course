d044095b6097c1e3ee02e12c653e2c283720ed90
gbilyuko <gbilyukov@hhogdev.com>
2015-01-08 15:22:26 +0200

    Added oop-in-use presentation

---
+++ examples/14.oop-in-use/DB_Config.php
@@ -0,0 +1,13 @@
+<?php
+	if(!isset($db_config)){
+
+		$db_config = array();		
+		
+		
+		//Dabase Configuration
+		$db_config["SERVER"] =  "localhost";
+		$db_config["USERNAME"] =  "sqluser";
+		$db_config["PASSWORD"] =  "123456";
+		$db_config["DABASE"] =  "world";
+	}
+?>
---
+++ examples/14.oop-in-use/index.php
@@ -0,0 +1,42 @@
+﻿<html>
+<head lang="en">
+    <meta charset="UTF-8">
+    <title>PHP OOP</title>
+</head>
+
+<body>
+<table border="1">
+	<thead>
+		<tr>
+			<th>Код</th>
+			<th>Име</th>
+			<th>Континент</th>
+			<th>Площ</th>
+		</tr>
+	</thead>
+
+<?php
+	require_once(dirname(__FILE__).'/DB.php');
+	
+	$db = DB::getInstance();
+	
+	$query = 'SELECT Code, Name, Continent, SurfaceArea ' .
+			 'FROM country ' .
+			 'WHERE Continent = "South America"';
+	
+	$results = $db->getQueryResults($query);
+	
+	foreach ($results as $res){
+		echo "<tr>";
+		echo "<td>" . $res['Code'] . "</td>";
+		echo "<td>" . $res['Name'] . "</td>";
+		echo "<td>" . $res['Continent'] . "</td>";
+		echo "<td>" . $res['SurfaceArea'] . "</td>";
+		echo "</tr>";
+	}
+?>
+
+</table>
+
+</body>
+</html>
