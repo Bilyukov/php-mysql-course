3b79ac1914add22105d67916145c37697167d9ce
Georgi Bilyukov <georgi.bilyukov@gmail.com>
2015-01-19 22:29:30 +0200

    Added example content

---
+++ examples/15.news-site-exercise/news-site/DB_Config.php
@@ -0,0 +1,13 @@
+<?php
+	if(!isset($db_config)){
+
+		$db_config = array();		
+		
+		
+		//Dabase Configuration
+		$db_config["SERVER"] =  "localhost";
+		$db_config["USERNAME"] =  "root";
+		$db_config["PASSWORD"] =  "";
+		$db_config["DABASE"] =  "newsportal";
+	}
+?>
